0bde35e89c72798e49d6f6ea4e5fe4b8c0a8578a
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-08-09 00:41:29 +0600

    Weather API is implemented in JS file

--- Making a Weather App using HTML,CSS & JS/weather.js
+++ Making a Weather App using HTML,CSS & JS/weather.js
@@ -1,6 +1,21 @@
 const loc = document.getElementById('location');
 function startsearch(){
   let searchqueary = document.getElementById('searchtext').value;
+  if(searchqueary === ''){
+    alert('Please enter a location');
+    return;
+  }
   loc.innerHTML = searchqueary;
   document.getElementById('searchtext').value = '';
+  api(searchqueary);
+  
 }
+
+function api(city)
+{
+  fetch('http://api.weatherapi.com/v1/current.json?key=ab929d9e99054f30a08155141240808&q='+city)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+}
+
